e2627020180f0cbfaccf00c1ae4582f9c54751de
Iza <isabela.ann@gmail.com>
2020-04-25 10:39:24 +0200

    changes in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1> Twój adres email to {{ email }} </h1>
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+  
+    <input type = "email" 
+          v-model = "email" >
+
+    <button @click="alertMyEmail()">Wchodzę</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// {atrybut: wartość, atrybut2:wartość2}
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+ data () {
+   return {
+     email: " "
+    };
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    },
+    hello() {
+
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
